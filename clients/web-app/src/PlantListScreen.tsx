af15e87afb7338ca37133a68c71a9d83047e08a8
Will Madden <w.a.madden@gmail.com>
2020-04-16 16:15:26 +0200

    Allow marking a plant as watered

--- clients/web-app/src/PlantListScreen.tsx
+++ clients/web-app/src/PlantListScreen.tsx
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import { Plant } from './state/dataTypes';
-import { List, ListItem, ListItemText, Container, Divider, Icon, ListItemIcon, Tooltip } from '@material-ui/core';
-import { Collection } from './state/useCollection';
+import {
+  List,
+  ListItem,
+  ListItemText,
+  Container,
+  Divider,
+  Icon,
+  ListItemIcon,
+  Tooltip,
+  ListItemSecondaryAction,
+  IconButton,
+} from '@material-ui/core';
+import { Collection, UpdateElement } from './state/useCollection';
 import partition from 'lodash/partition';
-import { green } from '@material-ui/core/colors';
 
 export type PlantListScreenProps = {
   plants: Collection<Plant>;
@@ -15,33 +25,53 @@ function needsWatering(plant: Plant, now = new Date(Date.now())) {
   return timeSinceWatering > wateringPeriodInMs;
 }
 
+const UnwateredPlantListItem: React.FC<{
+  plant: Plant;
+  updatePlant: UpdateElement<Plant>;
+}> = ({ plant, updatePlant }) => {
+  return (
+    <ListItem button>
+      <Tooltip title="Needs to be watered">
+        <ListItemIcon>
+          <Icon color="primary">opacity</Icon>
+        </ListItemIcon>
+      </Tooltip>
+      <ListItemText secondary={`Last watered ${plant.lastWateredAt.toLocaleDateString()}`}>{plant.name}</ListItemText>
+      <ListItemSecondaryAction
+        onClick={useCallback(() => {
+          updatePlant(plant, { ...plant, lastWateredAt: new Date() });
+        }, [plant, updatePlant])}
+      >
+        <IconButton edge="end" aria-label="done">
+          <Icon>check</Icon>
+        </IconButton>
+      </ListItemSecondaryAction>
+    </ListItem>
+  );
+};
+
 const PlantListScreen: React.FC<PlantListScreenProps> = ({ plants }) => {
-  const [plantsNeedingWatering, otherPlants] = partition(plants.data, needsWatering);
+  const [unwateredPlants, wateredPlants]: [Plant[], Plant[]] = partition<Plant>(plants.data, needsWatering);
 
   return (
     <Container>
-      {plantsNeedingWatering.length > 0 && (
+      {unwateredPlants.length > 0 && (
         <List>
-          {plants.data.map((plant) => (
-            <ListItem button key={plant.id}>
-              <Tooltip title="Needs to be watered">
-                <ListItemIcon>
-                  <Icon color="primary">opacity</Icon>
-                </ListItemIcon>
-              </Tooltip>
-              <ListItemText>{plant.name}</ListItemText>
-            </ListItem>
+          {unwateredPlants.map((plant) => (
+            <UnwateredPlantListItem key={plant.id} plant={plant} updatePlant={plants.dispatch.updateElement} />
           ))}
         </List>
       )}
 
-      {plantsNeedingWatering.length > 0 && otherPlants.length > 0 && <Divider />}
+      {unwateredPlants.length > 0 && wateredPlants.length > 0 && <Divider />}
 
-      {otherPlants.length > 0 && (
+      {wateredPlants.length > 0 && (
         <List>
-          {plants.data.map((plant) => (
+          {wateredPlants.map((plant) => (
             <ListItem button key={plant.id}>
-              <ListItemText>{plant.name}</ListItemText>
+              <ListItemText secondary={`Last watered ${plant.lastWateredAt.toLocaleDateString()}`}>
+                {plant.name}
+              </ListItemText>
             </ListItem>
           ))}
         </List>
