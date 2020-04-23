fdd5277e46881506f5ae21507da57bce190c888b
Will Madden <w.a.madden@gmail.com>
2020-04-23 15:24:12 +0200

    Add `date-fns` package

--- clients/web-app/src/plants/formatNextWaterDate.tsx
+++ clients/web-app/src/plants/formatNextWaterDate.tsx
@@ -1,8 +1,9 @@
+import add from 'date-fns/add';
 import { Plant, lastWateredAt } from '../data/Plant';
 
 export function formatNextWaterDate(plant: Plant) {
   const lastWateredAtDate = lastWateredAt(plant);
   if (!lastWateredAtDate) return 'Water today';
-  const nextWaterDate = new Date(lastWateredAtDate.valueOf() + plant.wateringPeriodInDays * 24 * 60 * 60 * 1000);
+  const nextWaterDate = add(lastWateredAtDate, { days: plant.wateringPeriodInDays });
   return `Water next on ${nextWaterDate.toLocaleDateString()}`;
 }
