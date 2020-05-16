e6e37cb7c83b9099eaf32f272c915c61bfd8b1fc
Will Madden <w.a.madden@gmail.com>
2020-05-16 17:25:07 +0200

    Add <Surface>
    Not sure how useful it is because RMWC's `<Theme>` component doesn't seem to have much effect on things nested inside it

---
+++ clients/web-app/src/components/Surface.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { ComponentProps } from '@rmwc/types';
+import '@rmwc/elevation/styles';
+import { ElevationProps, Elevation } from '@rmwc/elevation';
+import { Theme } from '@rmwc/theme';
+
+export type SurfaceProps = Omit<ComponentProps<ElevationProps, React.HTMLProps<HTMLElement>, 'div'>, 'tag' | 'ref'>;
+
+const Surface: React.FC<SurfaceProps> = ({ children, className, ...elevationProps }) => {
+  return (
+    <Elevation {...elevationProps}>
+      <Theme use={['surface', 'onSurface']}>{children}</Theme>
+    </Elevation>
+  );
+};
+
+export default Surface;
