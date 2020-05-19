98f772292b4fb4c360655dddbcb7a97cb52dbb9b
Will Madden <w.a.madden@gmail.com>
2020-05-19 12:25:58 +0200

    Add `parseCSV()`

---
+++ clients/web-app/src/data/parseCSV.ts
@@ -0,0 +1,17 @@
+import { Plant } from './Plant';
+
+export default function parseCSV(csvContent: string): Omit<Plant, 'id'> {
+  const lines = csvContent.split('\n').slice(1);
+  return lines.map((line) => {
+    const cells = line.split(',');
+
+    return {
+      name: cells[1],
+      wateringPeriodInDays: parseInt(cells[2], 10),
+      wateringTimes: cells[3]
+        .slice(1, -1)
+        .split(';')
+        .map((dateString) => new Date(Date.parse(dateString))),
+    };
+  });
+}
