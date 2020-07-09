00fb37c2b028b73e8ff023091bd0dc711cd7e077
Will Madden <w.a.madden@gmail.com>
2020-07-09 20:00:05 +0200

    Remove junk comment

--- clients/web-app/src/data/exportData.ts
+++ clients/web-app/src/data/exportData.ts
@@ -8,23 +8,5 @@ export type DataExport = {
 };
 
 export default function exportData(data: DataExport) {
-  // const headerRow = [
-  //   "id",
-  //   "name",
-  //   "wateringPeriodInDays",
-  //   "wateringTimes",
-  //   "timeOfDeath",
-  // ];
-  // const bodyRows = plants.map((plant) => [
-  //   plant.id,
-  //   plant.name,
-  //   plant.wateringPeriodInDays,
-  //   `"${plant.wateringTimes.map((date) => date.toISOString()).join(";")}"`,
-  //   plant.timeOfDeath ? plant.timeOfDeath.toISOString() : "",
-  // ]);
-  // const content = [headerRow, ...bodyRows].join("\n");
-
-  // return new Blob([content], { type: "text/csv;charset=utf-8" });
-
   return JSON.stringify(data);
 }
