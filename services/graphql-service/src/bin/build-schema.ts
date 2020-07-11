a8638d8f86fcbe6e72f5886182b1c361c578515a
Will Madden <w.a.madden@gmail.com>
2020-07-11 18:42:06 +0200

    Remove obsolete graphql-service

--- services/graphql-service/src/bin/build-schema.ts
+++
@@ -1,8 +0,0 @@
-import 'reflect-metadata';
-import { buildSchema } from 'type-graphql';
-import PlantResolver from '../resolvers/PlantResolver';
-
-buildSchema({
-  resolvers: [PlantResolver],
-  emitSchemaFile: true,
-});
