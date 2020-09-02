102c75562a5ac4fb658c8ce5a77761da0b20dd7c
Will Madden <w.a.madden@gmail.com>
2020-09-02 12:22:28 +0200

    Add utility `blindCast()`

---
+++ web-client/src/utilities/lang/blindCast.tsx
@@ -0,0 +1,19 @@
+/**
+ * Casts the input type to the `TargetType` with no type checks (i.e. blindly), as opposed to `castAs()` where the
+ * compiler will still prevent converting between incompatible types.
+ *
+ * The second type parameter is a string describing the reason you want to override the type system. This string is
+ * only present at compile-time and won't be output in the final JavaScript.
+ *
+ * Example:
+ *
+ *     const stringValue = blindCast<
+ *       string,
+ *       "string type enforced in function signature but unavailable here"
+ *     >(input[key]);
+ */
+export default function blindCast<TargetType, Reason extends string>(
+  input: any,
+): TargetType {
+  return input;
+}
