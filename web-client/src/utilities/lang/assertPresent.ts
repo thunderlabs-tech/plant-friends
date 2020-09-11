137fc54488ad212c5b3198f8238a51a1625ebe14
Will Madden <w.a.madden@gmail.com>
2020-09-11 11:35:21 +0200

    Convert `assertPresent()` to assertion function

--- web-client/src/utilities/lang/assertPresent.ts
+++ web-client/src/utilities/lang/assertPresent.ts
@@ -1,4 +1,5 @@
-export default function assertPresent<T>(value: T | undefined | null): T {
+export default function assertPresent<T>(
+  value: T | undefined | null,
+): asserts value is T {
   if (!value) throw new Error("Value must be present but isn't");
-  return value;
 }
