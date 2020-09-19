e739d950654e9458d912d36a29cf6d37a185dd2f
Will Madden <w.a.madden@gmail.com>
2020-09-19 15:25:56 +0200

    Add `utilities/i18n.ts` with date formatters

---
+++ web-client/src/utilities/i18n.ts
@@ -0,0 +1,28 @@
+import deepFreeze from "deep-freeze";
+import { mapValues } from "lodash";
+import castAs from "./lang/castAs";
+
+const locale = undefined; // TODO: detect locale; undefined uses browser default
+
+export const FORMATS = deepFreeze({
+  dateTime: {
+    dateTime: castAs<Partial<Intl.DateTimeFormatOptions>>({
+      year: "numeric",
+      month: "2-digit",
+      day: "2-digit",
+      hour: "2-digit",
+      minute: "2-digit",
+      hour12: false,
+    }),
+    date: castAs<Partial<Intl.DateTimeFormatOptions>>({
+      year: "numeric",
+      month: "2-digit",
+      day: "2-digit",
+    }),
+  },
+});
+
+export const dateFormatters = mapValues(
+  FORMATS.dateTime,
+  (format) => new Intl.DateTimeFormat(locale, format),
+);
