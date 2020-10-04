825c9d6a475cc46a4bb336245925d9faab0d4ba7
Will Madden <w.a.madden@gmail.com>
2020-10-04 14:12:31 +0200

    Extract `PlantAvatar` colors to `theme`

--- web-client/src/init/theme.ts
+++ web-client/src/init/theme.ts
@@ -1,3 +1,6 @@
+import { Color } from "src/utilities/Color";
+import { NonEmptyArray } from "src/utilities/lang/NonEmptyArray";
+
 const backgroundColor = "#f2f8f8";
 const primaryColor = "#8ECAD2";
 const secondaryColor = "#73B6A9";
@@ -5,6 +8,8 @@ const secondaryColor = "#73B6A9";
 const primaryTextColorOnLight = "#555a70";
 const secondaryTextColorOnLight = "#7c7f87";
 export const inputBg = "#deecec";
+
+export const plantAvatarColors: NonEmptyArray<Color> = ["#cef0de"];
 
 const theme = {
   background: backgroundColor,
