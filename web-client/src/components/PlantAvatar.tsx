d243896148206f9aa604f11a3d810998912b609d
Will Madden <w.a.madden@gmail.com>
2020-10-04 14:12:31 +0200

    Extract `PlantAvatar` colors to `theme`

--- web-client/src/components/PlantAvatar.tsx
+++ web-client/src/components/PlantAvatar.tsx
@@ -3,15 +3,18 @@ import { Plant } from "src/data/Plant";
 import "@rmwc/avatar/styles";
 import { Avatar } from "@rmwc/avatar";
 import { grey } from "src/utilities/ColorPalettes";
+import { plantAvatarColors } from "src/init/theme";
 import getContrastTextColor from "src/utilities/getContrastTextColor";
 
 import css from "src/components/PlantAvatar.module.css";
+import { Color } from "src/utilities/Color";
+import { NonEmptyArray } from "src/utilities/lang/NonEmptyArray";
 
 export type PlantAvatarProps = {
   plant: Plant;
 };
 
-const availableColors: [string, ...string[]] = ["#cef0de"];
+const availableColors: NonEmptyArray<Color> = plantAvatarColors;
 
 const PlantAvatar: React.FC<PlantAvatarProps> = (props) => {
   const plant = props.plant;
