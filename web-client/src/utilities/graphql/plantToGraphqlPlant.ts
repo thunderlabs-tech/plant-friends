1c9ce003b2fcbd3ba96694160506dde5d4997cf3
Will Madden <w.a.madden@gmail.com>
2020-10-04 21:37:24 +0200

    Add new fields to `plantToGraphqlPlant()`

--- web-client/src/utilities/graphql/plantToGraphqlPlant.ts
+++ web-client/src/utilities/graphql/plantToGraphqlPlant.ts
@@ -4,13 +4,23 @@ import { Plant } from "src/data/Plant";
 
 export default function plantToGraphqlPlant(
   input: Plant,
-): Pick<Plant, "id" | "name" | "wateringPeriodInDays"> & {
+): Pick<
+  Plant,
+  "id" | "name" | "wateringPeriodInDays" | "fertilizingPeriodInDays"
+> & {
   lastWateredAt: string | null;
+  lastFertilizedAt: string | null;
   timeOfDeath: string | null;
 } {
   return {
-    ...pick(input, ["id", "name", "wateringPeriodInDays"]),
+    ...pick(input, [
+      "id",
+      "name",
+      "wateringPeriodInDays",
+      "fertilizingPeriodInDays",
+    ]),
     timeOfDeath: dateToString(input.timeOfDeath),
     lastWateredAt: dateToString(input.lastWateredAt),
+    lastFertilizedAt: dateToString(input.lastFertilizedAt),
   };
 }
