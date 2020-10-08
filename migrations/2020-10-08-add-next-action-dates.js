ad9bed3646408fbc56da98ef8b8b6a3d9e8b08c0
Will Madden <w.a.madden@gmail.com>
2020-10-08 20:09:58 +0200

    Add a script to populate existing plants with missing data

---
+++ migrations/2020-10-08-add-next-action-dates.js
@@ -0,0 +1,114 @@
+const AWS = require("aws-sdk");
+const dateFns = require("date-fns");
+
+function scanAllPlants(docClient, plantTableName, fn) {
+  return new Promise((resolve, reject) => {
+    docClient.scan({ TableName: plantTableName }, onScan);
+
+    function onScan(err, data) {
+      if (err) {
+        reject(err);
+      } else {
+        data.Items.forEach(fn);
+
+        if (typeof data.LastEvaluatedKey != "undefined") {
+          params.ExclusiveStartKey = data.LastEvaluatedKey;
+          docClient.scan(params, onScan);
+        } else {
+          resolve();
+        }
+      }
+    }
+  });
+}
+
+async function updatePlant(docClient, plantTableName, id, newValues) {
+  const keys = Object.keys(newValues);
+  const assignments = keys.map((key) => `${key} = :${key}`).join(", ");
+  const expressionAttributeValues = keys.reduce((result, key) => {
+    result[`:${key}`] = newValues[key];
+    return result;
+  }, {});
+
+  var params = {
+    TableName: plantTableName,
+    Key: {
+      id,
+    },
+    UpdateExpression: `set ${assignments}`,
+    ExpressionAttributeValues: expressionAttributeValues,
+    ReturnValues: "UPDATED_NEW",
+  };
+
+  console.log(params);
+
+  docClient.update(params, function (err, data) {
+    if (err) {
+      console.error(
+        "Unable to update item. Error JSON:",
+        JSON.stringify(err, null, 2)
+      );
+    } else {
+      console.log("UpdateItem succeeded:", JSON.stringify(data, null, 2));
+    }
+  });
+}
+
+function formatAwsDateString(date) {
+  if (date === null || date === undefined) return null;
+  return dateFns.format(date, "yyyy-MM-dd");
+}
+
+function parseDateString(dateStringOrUndefined) {
+  if (!dateStringOrUndefined) return undefined;
+  return new Date(Date.parse(dateStringOrUndefined));
+}
+
+function nextActionDueDate({ lastPerformedAt, plantCreatedAt, periodInDays }) {
+  if (periodInDays === null || periodInDays === undefined) return undefined;
+
+  const endOfPeriod = dateFns.add(lastPerformedAt || plantCreatedAt, {
+    days: periodInDays,
+  });
+
+  return dateFns.startOfDay(endOfPeriod);
+}
+
+function getWaterNextAt(plant) {
+  return nextActionDueDate({
+    lastPerformedAt: parseDateString(plant.lastWateredAt),
+    periodInDays: plant.wateringPeriodInDays,
+    plantCreatedAt: parseDateString(plant.createdAt),
+  });
+}
+
+function getFertilizeNextAt(plant) {
+  return nextActionDueDate({
+    lastPerformedAt: parseDateString(plant.lastFertilizedAt),
+    periodInDays: plant.fertilizingPeriodInDays,
+    plantCreatedAt: parseDateString(plant.createdAt),
+  });
+}
+
+async function main() {
+  AWS.config.update({ region: "eu-central-1" });
+
+  var docClient = new AWS.DynamoDB.DocumentClient();
+
+  const plantTableName = "Plant-h3ngh5kpofcmlhuukxvi6nk6qa-dev";
+
+  await scanAllPlants(docClient, plantTableName, (plant) => {
+    const waterNextAt = getWaterNextAt(plant);
+    const fertilizeNextAt = getFertilizeNextAt(plant);
+
+    updatePlant(docClient, plantTableName, plant.id, {
+      waterNextAt: formatAwsDateString(waterNextAt),
+      fertilizeNextAt: formatAwsDateString(fertilizeNextAt),
+    }).catch((error) => {
+      console.error(error);
+      process.exit(1);
+    });
+  });
+}
+
+main();
