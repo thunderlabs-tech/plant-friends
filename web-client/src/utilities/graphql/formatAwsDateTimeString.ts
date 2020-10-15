afcdabf0def7bad356ffe6416b58f42051108834
Will Madden <w.a.madden@gmail.com>
2020-10-15 22:08:08 +0200

    Rename function to match filename

--- web-client/src/utilities/graphql/formatAwsDateTimeString.ts
+++ web-client/src/utilities/graphql/formatAwsDateTimeString.ts
@@ -1,12 +1,14 @@
 import { formatISO } from "date-fns";
 /* eslint-disable no-redeclare */
 
-export default function dateToString(value: Date): string;
-export default function dateToString(value: Date | null): string | null;
-export default function dateToString(
+export default function formatAwsDateTimeString(value: Date): string;
+export default function formatAwsDateTimeString(
+  value: Date | null,
+): string | null;
+export default function formatAwsDateTimeString(
   value: Date | undefined,
 ): string | undefined;
-export default function dateToString(
+export default function formatAwsDateTimeString(
   value: Date | undefined | null,
 ): string | undefined | null {
   if (value) return formatISO(value);
